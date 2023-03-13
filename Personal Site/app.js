9e5afec4e1a34f9042a217c9a63db57c9b0c63bb
Dechsit Naetsawan <dechsitbkk@gmai.com>
2023-03-13 17:31:34 -0400

    Added navbar underline when scrolling

--- Personal Site/app.js
+++ Personal Site/app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const sections = document.querySelectorAll("section[id]");
 
 //Display Mobile Menu
 const mobileMenu = () => {
@@ -8,3 +9,45 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu)
+
+// Credit to https://gist.github.com/khuongyolo/4e7d8308c01733961f7ad0c9964db591 for navbar highlight on scroll
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+    // Get current scroll position
+    let scrollY = window.pageYOffset;
+    
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+  
+    // Original:
+    // const sectionTop = current.offsetTop - 50;
+    //  
+    // Alex Turnwall's update:
+    // Updated original line (above) to use getBoundingClientRect instead of offsetTop, based on:
+    // https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
+    // https://newbedev.com/difference-between-getboundingclientrect-top-and-offsettop
+    // This allows the use of sections inside a relative parent, which I'm not using here, but needed for a project
+    //
+    var sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 400;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navbar_container a[href*=" + sectionId + "]").classList.add("highlight");
+    } else {
+      document.querySelector(".navbar_container a[href*=" + sectionId + "]").classList.remove("highlight");
+    }
+  });
+}
+
+
